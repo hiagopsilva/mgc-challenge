61c7ca380aedcbd5b025ced3370760cd34c6be45
Hiago Prates <hiagohps360@gmail.com>
2022-08-25 17:43:57 -0300

    chore: update

--- src/contexts/auth.tsx
+++ src/contexts/auth.tsx
@@ -1,7 +1,7 @@
 import { createContext, ReactNode } from 'react';
 
 import { Storage } from '~/services';
-// import { UserAPI } from '~/services/API';
+import { UserAPI } from '~/services/API';
 
 export const AuthContext = createContext({} as UserType.AuthContextData);
 
@@ -13,12 +13,13 @@ export function AuthProvider({ children }: AuthProviderProps) {
   let isAuthenticated = false;
 
   const signIn = async (authFormData: UserType.Login) => {
-    // const response = await UserAPI.Auth({
-    //   user: authFormData.email,
-    //   pass: authFormData.password,
-    // });
+    const response = await UserAPI.Auth({
+      user: authFormData.email,
+      pass: authFormData.password,
+    });
 
     console.log(authFormData);
+    console.log(response);
     // Storage.setToken('123');
     isAuthenticated = true;
 

be0e8bfb8a80ef61fe490a2f554aa27d9463f97d
Hiago Prates <hiagohps360@gmail.com>
2022-08-25 15:28:11 -0300

    feat: context api

---
+++ src/contexts/auth.tsx
@@ -0,0 +1,33 @@
+import { createContext, ReactNode } from 'react';
+
+import { Storage } from '~/services';
+// import { UserAPI } from '~/services/API';
+
+export const AuthContext = createContext({} as UserType.AuthContextData);
+
+type AuthProviderProps = {
+  children: ReactNode;
+};
+
+export function AuthProvider({ children }: AuthProviderProps) {
+  let isAuthenticated = false;
+
+  const signIn = async (authFormData: UserType.Login) => {
+    // const response = await UserAPI.Auth({
+    //   user: authFormData.email,
+    //   pass: authFormData.password,
+    // });
+
+    console.log(authFormData);
+    // Storage.setToken('123');
+    isAuthenticated = true;
+
+    return true;
+  };
+
+  return (
+    <AuthContext.Provider value={{ isAuthenticated, signIn }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}

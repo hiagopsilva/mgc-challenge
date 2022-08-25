4e59429b8b3024d5d90d615a4bcb194b875f6ff8
Hiago Prates <hiagohps360@gmail.com>
2022-08-25 15:29:35 -0300

    chore: updated modules import

--- src/utils/modules/index.ts
+++ src/utils/modules/index.ts
@@ -5,7 +5,7 @@ import * as yup from 'yup';
 
 export { Toastify, ReactLoading, yup };
 
-export { observer, inject, MobXProviderContext, Provider } from 'mobx-react';
 export { default as styled } from 'styled-components';
 
 export * from 'formik';
+export * from 'history';

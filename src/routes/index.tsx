98ef418e3cfd859e663399e92606b5512414e152
Hiago Prates <hiagohps360@gmail.com>
2022-08-25 15:28:49 -0300

    fix: routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { Router } from 'react-router-dom';
+
+import { createBrowserHistory } from 'history';
 
 import { Home, Login } from '~/scenes';
 
@@ -6,11 +9,13 @@ import Guest from './guest';
 import Private from './private';
 import { Routing } from './routing';
 
+const browserHistory = createBrowserHistory();
+
 const RoutesContainer: React.FC = () => (
-  <>
+  <Router history={browserHistory}>
     <Guest exact path={Routing.LOGIN} component={Login} />
     <Private path={Routing.HOME} component={Home} />
-  </>
+  </Router>
 );
 
 export { Routing };

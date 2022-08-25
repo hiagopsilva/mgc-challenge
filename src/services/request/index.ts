4b9ffc123c6b07372fa865468b5a199ea2e04719
Hiago Prates <hiagohps360@gmail.com>
2022-08-25 18:13:27 -0300

    chore: update

--- src/services/request/index.ts
+++ src/services/request/index.ts
@@ -6,7 +6,6 @@ export const request = Axios.create({
   baseURL: REACT_APP_API_URL,
   headers: {
     accept: 'application/json',
-    'Access-Control-Allow-Origin': '*',
   },
   timeout: 15000, // 15 seconds timeout
 });

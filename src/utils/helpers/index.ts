714cf96b55aad30670f43a20fe50218a8795d698
Hiago Prates <hiagohps360@gmail.com>
2022-08-26 04:02:37 -0300

    feat: mask cpf

--- src/utils/helpers/index.ts
+++ src/utils/helpers/index.ts
@@ -1,3 +1,9 @@
 export * from './theme';
 
 export { default as alert } from './toast';
+
+export const formatCPF = (value: string) => {
+  const newValueCPF = value.replace(/[^\d]/g, '');
+
+  return newValueCPF.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4');
+};

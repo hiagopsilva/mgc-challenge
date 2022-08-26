f618accfa6d8a7407e7e69477e5ce84634f0ea57
Hiago Prates <hiagohps360@gmail.com>
2022-08-26 20:47:36 -0300

    refactor: card debts

--- src/atoms/Debts/index.tsx
+++ src/atoms/Debts/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { currencyFormat, formatDate } from '~/utils';
+import { currencyFormat, formatDate, hasValue } from '~/utils';
 
 import { Item, TextItem, Wrapper } from './styles';
 
@@ -13,14 +13,27 @@ const Debts: React.FC<Props> = ({ dataDebts = {} }) => {
     <Wrapper>
       {dataDebts.map((item: any) => (
         <Item key={item.id}>
-          <TextItem>{item.carteira}</TextItem>
-          <TextItem>{formatDate(item.data)}</TextItem>
-          <TextItem>{currencyFormat(item.valor)}</TextItem>
           <TextItem>
-            ATIVO:
-            {`${item.ativo}` ? ' SIM' : ' NÃO'}
+            <span className="label">Carteira: </span>
+            {item.carteira}
           </TextItem>
-          <TextItem>{item.bucket}</TextItem>
+          <TextItem>
+            <span className="label">Data: </span>
+
+            {formatDate(item.data)}
+          </TextItem>
+          <TextItem>
+            <span className="label">R$: </span>
+            {currencyFormat(item.valor)}
+          </TextItem>
+          <TextItem>
+            <span className="label">Ativo: </span>
+            {hasValue(item.ativo)}
+          </TextItem>
+          <TextItem>
+            <span className="label">Bucket: </span>
+            {item.bucket}
+          </TextItem>
         </Item>
       ))}
     </Wrapper>
--- src/atoms/Debts/styles.ts
+++ src/atoms/Debts/styles.ts
@@ -4,30 +4,45 @@ import { getTheme, pxToRem } from '~/utils';
 
 const primaryBorder = getTheme('primary.border');
 const primaryDark = getTheme('primary.dark');
+const tertiaryMain = getTheme('tertiary.main');
+
+const inMobile = getTheme('inMobile');
 
 export const Wrapper = styled.div`
   width: 95%;
 
   margin: ${pxToRem(20)} auto ${pxToRem(20)};
 
-  flex: 1;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
 `;
 
 export const Item = styled.div`
-  width: 100%;
-  height: ${pxToRem(50)};
+  width: 49%;
+  min-height: ${pxToRem(50)};
   margin-bottom: ${pxToRem(12)};
-  padding: 0 ${pxToRem(24)};
+  padding: ${pxToRem(12)} ${pxToRem(12)};
 
   border: 1px solid ${primaryBorder};
   border-radius: ${pxToRem(4)};
 
   display: flex;
   flex-direction: row;
+  flex-wrap: wrap;
   justify-content: space-between;
   align-items: center;
 `;
 
 export const TextItem = styled.span`
   color: ${primaryDark};
+
+  width: 49%;
+
+  margin-bottom: 4px;
+
+  span.label {
+    font-weight: bold;
+    color: ${tertiaryMain};
+  }
 `;

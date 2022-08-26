cab5d6a050d417b93bd166a98707f94d4bc0c4f3
Hiago Prates <hiagohps360@gmail.com>
2022-08-26 19:42:12 -0300

    fix: router

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,18 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch } from 'react-router-dom';
 
-import { createBrowserHistory } from 'history';
-
 import { Home, Login } from '~/scenes';
 
 import Guest from './guest';
 import Private from './private';
 import { Routing } from './routing';
 
-const browserHistory = createBrowserHistory();
-
 const RoutesContainer: React.FC = () => (
-  <Router history={browserHistory}>
+  <Router>
     <Switch>
       <Guest exact path={Routing.LOGIN} component={Login} />
       <Private path={Routing.HOME} component={Home} />

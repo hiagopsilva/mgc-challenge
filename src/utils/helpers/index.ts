5d0d337e1cb8ec1f8254960a0c6261bd2e4422bc
Hiago Prates <hiagohps360@gmail.com>
2022-08-27 00:38:14 -0300

    fix: format money

--- src/utils/helpers/index.ts
+++ src/utils/helpers/index.ts
@@ -24,7 +24,7 @@ export const formatDate = (date: string, formatDateCompleted?: boolean) => {
 };
 
 export const currencyFormat = (num: any) => {
-  return `${num.toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')}`;
+  return `${num.toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.')}`;
 };
 
 export const hasValue = (value: boolean) => (value ? ' Sim' : ' Não');
